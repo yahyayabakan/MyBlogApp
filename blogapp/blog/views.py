51166ce1ea4097784f7e66d86a8e23322ab7f335
yahyayabakan <yahyaayabakan007@gmail.com>
2022-04-22 11:33:23 +0100

    finished project

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,15 +1,31 @@
-from django.http import HttpResponse
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
-
-# Create your views here.
+from blog.models import Blog, Category
 
 def index(request):
-    return render(request, "blog/index.html")
+    context = {
+        "blogs": Blog.objects.filter(is_active=True, is_home=True),
+        "categories": Category.objects.all()
+    }
+    return render(request, "blog/index.html", context)
 
 def blogs(request):
-    return render(request, "blog/blogs.html")
+    context = {
+        "blogs": Blog.objects.filter(is_active=True),
+        "categories": Category.objects.all()
+    }
+    return render(request, "blog/blogs.html", context)
 
-def blog_details(request, id):
-    return render(request, "blog/blog-details.html", {'id':id})
-        
+def blog_details(request, slug):
+    blog = Blog.objects.get(slug=slug)
+    return render(request, "blog/blog-details.html", {
+        "blog": blog
+    })
+
+def blogs_by_category(request, slug):
+    context = {
+        "blogs": Category.objects.get(slug=slug).blog_set.filter(is_active=True),
+        "categories": Category.objects.all(),
+        "selected_category": slug
+    }
+    return render(request, "blog/blogs.html", context)
--- blogapp/blogapp/urls.py
+++ blogapp/blogapp/urls.py
@@ -1,5 +1,4 @@
 """blogapp URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -13,11 +12,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
-]
+    path('', include('blog.urls')),
+    path('account/', include('account.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
